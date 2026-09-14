42b17efbe9edcf7a1c689a5817b9d8e75009fd7c
stevecopal <copalsteve@gmail.com>
2026-09-14 12:26:28 +0100

    page d'erreur

--- autolink/settings.py
+++ autolink/settings.py
@@ -9,7 +9,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me-in-production")
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
 
--- autolink/urls.py
+++ autolink/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.defaults import page_not_found, server_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,3 +20,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Pages d'erreur pour la production (DEBUG=False)
+handler404 = page_not_found
+handler500 = server_error
+
