ba2c69823abbbace7b5245cb3e5418a1ea24c094
stevecopal <copalsteve@gmail.com>
2026-09-15 14:27:54 +0100

    probleme de bd en prod

--- autolink/settings.py
+++ autolink/settings.py
@@ -99,7 +99,7 @@ else:
             default=config("DATABASE_URL", default=""),
             conn_max_age=600,
             conn_health_checks=True,
-            ssl_require=True,
+            ssl_require=False,
         )
     }
 
